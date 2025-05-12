b4777987a9f169fdffd29566c38c267250b17fb2
Rubén D. Guerrero N. <rudargeneira@gmail.com>
2025-05-12 16:31:46 +0200

    feat: add multer for file upload handling

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer')
 require('dotenv').config()
 
 var app = express();
+var upload = multer({ dest: 'uploads/' });
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res) {
+  if (!req.file) {
+    return res.status(400).json({ error: 'No file uploaded' });
+  }
+
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  });
 });
 
 
